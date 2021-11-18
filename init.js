fe124696445cdeb0a09eeef126d51777f4aaccce
Stackbit <projects@stackbit.com>
2021-11-18 11:11:55 +0000

    initial commit by Stackbit

---
+++ init.js
@@ -0,0 +1,10 @@
+import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
+
+export const PLASMIC = initPlasmicLoader({
+  projects: [
+    {
+      id: "gbKas2X3nUwmvdo8faEPzB",
+      token: "8SEiKpxfGs1m3u9oNxmL6TVrxIwPy00zQjORpzPtnbkQhkmWKL2xPya9bE9Ugp9QlHloik4oISgMUrrmIinw",
+    },
+  ],
+});
